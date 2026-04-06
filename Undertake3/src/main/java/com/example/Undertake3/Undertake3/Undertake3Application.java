6f0bfcf86bcae76bfb83b2a20f93ff0a9b53690a
Alejandro Enriquez Botina <a.enriquez@unimayor.edu.co>
2026-04-06 14:07:19 -0500

    oliwi

---
+++ Undertake3/src/main/java/com/example/Undertake3/Undertake3/Undertake3Application.java
@@ -0,0 +1,13 @@
+package com.example.Undertake3.Undertake3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Undertake3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Undertake3Application.class, args);
+	}
+
+}
---
+++ Undertake3/src/test/java/com/example/Undertake3/Undertake3/Undertake3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Undertake3.Undertake3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Undertake3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
